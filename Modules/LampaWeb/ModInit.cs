1e50abff24c4a85a8f25ff0484e5f0b861d73712
ngcores <91776825+ngcores@users.noreply.github.com>
2026-04-25 11:18:25 +0300

    Update lampa version

--- Modules/LampaWeb/ModInit.cs
+++ Modules/LampaWeb/ModInit.cs
@@ -5,51 +5,48 @@ using Shared.Models.Events;
 using Shared.Models.Module;
 using Shared.Models.Module.Interfaces;
 using System.Collections.Generic;
-using LampaWeb.Models;
-using LampaWeb.Services;
 
-namespace LampaWeb
+namespace LampaWeb;
+
+public class ModInit : IModuleLoaded
 {
-    public class ModInit : IModuleLoaded
+    public static string modpath;
+
+    public static ModuleConf conf;
+
+    public void Loaded(InitspaceModel baseconf)
     {
-        public static string modpath;
+        modpath = baseconf.path;
 
-        public static ModuleConf conf;
+        updateConf();
+        EventListener.UpdateInitFile += updateConf;
 
-        public void Loaded(InitspaceModel baseconf)
+        foreach (var m in conf.limit_map)
+            CoreInit.conf.WAF.limit_map.Insert(0, m);
+
+        LampaCron.Start();
+    }
+
+    public void Dispose()
+    {
+        LampaCron.Stop();
+        EventListener.UpdateInitFile -= updateConf;
+    }
+
+    void updateConf()
+    {
+        conf = ModuleInvoke.Init("LampaWeb", new ModuleConf()
         {
-            modpath = baseconf.path;
-
-            updateConf();
-            EventListener.UpdateInitFile += updateConf;
-
-            foreach (var m in conf.limit_map)
-                CoreInit.conf.WAF.limit_map.Insert(0, m);
-
-            LampaCron.Start();
-        }
-
-        public void Dispose()
-        {
-            LampaCron.Stop();
-            EventListener.UpdateInitFile -= updateConf;
-        }
-
-        void updateConf()
-        {
-            conf = ModuleInvoke.Init("LampaWeb", new ModuleConf()
+            autoupdate = true,
+            intervalupdate = 90, // minute
+            basetag = true,
+            index = "lampa-main/index.html",
+            git = "yumata/lampa",
+            tree = "9e99655e7fcdb378825987265cf07e586256fa34",
+            limit_map = new List<WafLimitRootMap>()
             {
-                autoupdate = true,
-                intervalupdate = 90, // minute
-                basetag = true,
-                index = "lampa-main/index.html",
-                git = "yumata/lampa",
-                tree = "a30c9852d8722e4b3a058460ee38bb7193816e21",
-                limit_map = new List<WafLimitRootMap>()
-                {
-                    new("^/(extensions|testaccsdb|msx/)", new WafLimitMap { limit = 10, second = 1 })
-                }
-            });
-        }
+                new("^/(extensions|testaccsdb|msx/)", new WafLimitMap { limit = 10, second = 1 })
+            }
+        });
     }
 }
