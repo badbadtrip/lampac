07674f108588e6c17838856648c2d5afe8229ea3
ngcores <91776825+ngcores@users.noreply.github.com>
2026-04-27 16:11:06 +0300

    lowMemoryMode

--- Shared/Services/Utilities/GeoIP2.cs
+++ Shared/Services/Utilities/GeoIP2.cs
@@ -1,4 +1,5 @@
-﻿using MaxMind.GeoIP2;
+﻿using MaxMind.Db;
+using MaxMind.GeoIP2;
 
 namespace Shared.Services.Utilities;
 
@@ -6,16 +7,24 @@ public class GeoIP2
 {
     static readonly Lazy<DatabaseReader> cityReader = new Lazy<DatabaseReader>(() =>
     {
+        var mode = CoreInit.conf.lowMemoryMode
+            ? FileAccessMode.MemoryMapped
+            : FileAccessMode.Memory;
+
         if (File.Exists("data/GeoLite2-Country.mmdb"))
-            return new DatabaseReader("data/GeoLite2-Country.mmdb");
+            return new DatabaseReader("data/GeoLite2-Country.mmdb", mode);
 
         return null;
     });
 
     static readonly Lazy<DatabaseReader> asnReader = new Lazy<DatabaseReader>(() =>
     {
+        var mode = CoreInit.conf.lowMemoryMode
+            ? FileAccessMode.MemoryMapped
+            : FileAccessMode.Memory;
+
         if (File.Exists("data/GeoLite2-ASN.mmdb"))
-            return new DatabaseReader("data/GeoLite2-ASN.mmdb");
+            return new DatabaseReader("data/GeoLite2-ASN.mmdb", mode);
 
         return null;
     });
